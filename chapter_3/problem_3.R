2acab967fd185553a07c50c80f94766c6d7076a1
andrew pappas <aragacalledpappas@gmail.com>
2024-03-23 14:30:43 -0400

    problem 3

--- chapter_3/problem_3.R
+++ chapter_3/problem_3.R
@@ -2,5 +2,10 @@
 # 1. What are the numeric values of its parameters? That is,
 # U(0, 1) = Beta(?, ?)
 
+#beta(1,1) = [gamma(2)/(gamma(1) + gamma(1))] * x^0 * (1-x)^0
+# each term equals 1
 
 # 2. What is the equivalent prior sample size for a U(0, 1) prior?
+
+#it's the equivelent of a sample size of two, with one 'success'....basically a .5 ratio,
+#sort of like assuming nothing in the beginnings
